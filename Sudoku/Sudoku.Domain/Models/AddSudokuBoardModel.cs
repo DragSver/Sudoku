554c3389ce02c538585f63a5bfbe559f9f8f9dc1
Olga Modina <o.modina@finiki.games>
2024-01-23 17:23:06 +0500

    Add models and responses for SudokuBoard

---
+++ Sudoku/Sudoku.Domain/Models/AddSudokuBoardModel.cs
@@ -0,0 +1,12 @@
+﻿using Sudoku.Domain.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace Sudoku.Domain.Models;
+
+public class AddSudokuBoardModel
+{
+    [Required]
+    public SudokuUser SudokuUser { get; set; }
+    [Required(AllowEmptyStrings = false)]
+    public string SudokuBoardData { get;set; }
+}
---
+++ Sudoku/Sudoku.Domain/Response/GetSudokuBoardsOfUser.cs
@@ -0,0 +1,8 @@
+﻿using Sudoku.Domain.Entities;
+
+namespace Sudoku.Domain.Response;
+
+public class GetSudokuBoardsOfUser
+{
+    public List<SudokuBoard> SudokuBoards { get; set; }
+}
