5b4868886fef0f4d890de8becc4282213fc1ac83
Olga Modina <o.modina@finiki.games>
2024-01-23 18:32:17 +0500

    Add SudokuBoard request handlers

---
+++ Sudoku/Sudoku.BL/AddSudokuBoardRequestHandler.cs
@@ -0,0 +1,38 @@
+﻿using MediatR;
+using Sudoku.DataAccess;
+using Sudoku.Domain.Entities;
+
+namespace Sudoku.BL;
+
+public class AddSudokuBoardRequest : IRequest<Guid?>
+{
+    public string SudokuBoardData { get; set; }
+    public SudokuUser User { get; set; }
+}
+
+public class AddSudokuBoardRequestHandler : IRequestHandler<AddSudokuBoardRequest, Guid?>
+{
+    private readonly AppDbContext _appDbContext;
+
+    public AddSudokuBoardRequestHandler(AppDbContext appDbContext)
+    {
+        _appDbContext = appDbContext;
+    }
+
+    public async Task<Guid?> Handle(AddSudokuBoardRequest request, CancellationToken cancellationToken)
+    {
+        var entity = new SudokuBoard { SudokuBoardData = request.SudokuBoardData, User = request.User };
+
+        try
+        {
+            await _appDbContext.AddAsync(entity, cancellationToken);
+            await _appDbContext.SaveChangesAsync();
+
+            return entity.Id;
+        }
+        catch (Exception ex)
+        {
+            return null;
+        }
+    }
+}
--- Sudoku/Sudoku.BL/GetSudokuUserRequestHandler.cs
+++ Sudoku/Sudoku.BL/GetSudokuUserRequestHandler.cs
@@ -1,13 +1,7 @@
 ﻿using MediatR;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 using Sudoku.DataAccess;
 using Sudoku.Domain.Response;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Sudoku.BL;
 
