c0fe23d4b9c3b0ae026b8ffaac49d70e56567084
Olga Modina <o.modina@finiki.games>
2024-01-23 17:12:15 +0500

    Add SudokuBoard entity, edit DbContext

---
+++ Sudoku/Sudoku.Domain/Entities/SudokuBoard.cs
@@ -0,0 +1,10 @@
+﻿namespace Sudoku.Domain.Entities;
+
+public class SudokuBoard
+{
+    public Guid Id { get; set; }
+    public string SudokuBoardData { get; set; }
+
+    public Guid UserId { get; set; }
+    public SudokuUser User { get; set; }
+}
